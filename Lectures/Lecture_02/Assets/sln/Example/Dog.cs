9afe66c1ed9f769817fd5711db85d90fad7a0e28
Jan Pluskal <ipluskal@fit.vutbr.cz>
2019-02-09 15:52:25 +0100

    L2

--- Lectures/Lecture_02/Assets/sln/Example/Dog.cs
+++ Lectures/Lecture_02/Assets/sln/Example/Dog.cs
@@ -2,14 +2,14 @@
 {
     public class Dog: Animal
     {
-        string coatColor;
-        int length;
+        string coatColor;           // Class member - field
+        int length;                 // Class member - field
 
-        public override string Cry()
+        public override string Cry()// Class member - method
         {
             return "Woof!";
         }
 
-        public void Bite() { }
+        public void Bite() { }      // Class member - method
     }
 }
