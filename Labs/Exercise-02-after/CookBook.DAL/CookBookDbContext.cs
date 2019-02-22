2cd57d3ea680993293e80391807a5dc07ba55361
Tibor Jašek <tibor.jasek@riganti.cz>
2019-02-22 12:17:43 +0100

    Added Exercise 02 before and after.

---
+++ Labs/Exercise-02-after/CookBook.DAL/CookBookDbContext.cs
@@ -0,0 +1,29 @@
+﻿using CookBook.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CookBook.DAL
+{
+    public class CookBookDbContext : DbContext
+    {
+        public CookBookDbContext()
+        {
+
+        }
+        public CookBookDbContext(DbContextOptions<CookBookDbContext> contextOptions)
+            : base(contextOptions)
+        {
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(
+        //        @"Data Source=(LocalDB)\MSSQLLocalDB;
+        //        Initial Catalog = CookBook;
+        //        MultipleActiveResultSets = True;
+        //        Integrated Security = True; ");
+        //}
+
+        public DbSet<RecipeEntity> Recipes { get; set; }
+        public DbSet<IngredientEntity> Ingredients { get; set; }
+    }
+}
