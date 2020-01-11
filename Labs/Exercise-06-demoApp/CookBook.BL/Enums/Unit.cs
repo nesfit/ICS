acf0b968e0d227024e38ee5594653c0c7cb444f6
Jan Pluskal <jan.pluskal@gmail.com>
2020-01-11 10:41:39 +0100

    Add enum descriptions using System.ComponentModel.EnumConverter.

--- Labs/Exercise-06-demoApp/CookBook.BL/Enums/Unit.cs
+++ Labs/Exercise-06-demoApp/CookBook.BL/Enums/Unit.cs
@@ -1,12 +1,22 @@
-﻿namespace CookBook.BL.Enums
+﻿using System.ComponentModel;
+using CookBook.BL.Converters;
+
+namespace CookBook.BL.Enums
 {
+    [TypeConverter(typeof(EnumDescriptionTypeConverter))]
     public enum Unit
     {
+        [Description("kg")]
         Kg,
+        [Description("l")]
         L,
+        [Description("ml")]
         Ml,
+        [Description("g")]
         G,
+        [Description("pieces")]
         Pieces,
+        [Description("spoon")]
         Spoon
     }
 }
