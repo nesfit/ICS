a6684d290c6b41220d53721da41be5a7949a347d
Jan Pluskal <jan.pluskal@gmail.com>
2021-02-14 16:41:12 +0100

    Update EF Core 5.

--- Labs/Exercise-02-after/CookBook.DAL/Factories/IDbContextFactory.cs
+++ Labs/Exercise-02-after/CookBook.DAL/Factories/IDbContextFactory.cs
@@ -2,7 +2,7 @@
 
 namespace CookBook.DAL.Factories
 {
-    public interface IDbContextFactory<out TDbContext> where TDbContext : DbContext
+    public interface INamedDbContextFactory<out TDbContext> where TDbContext : DbContext
     {
         TDbContext Create();
     }
