069859bdd69e2d7d22ef5337abf204767dc18a04
Erik Machacek <erik.machacek@solarwinds.com>
2021-05-03 16:05:12 +0200

    Initial import of L12 for 2021

---
+++ Lectures/Lecture_12/SelfContainedApp/SelfContainedApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SelfContainedApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Press any key to exit program");
+            Console.ReadKey();
+        }
+    }
+}
