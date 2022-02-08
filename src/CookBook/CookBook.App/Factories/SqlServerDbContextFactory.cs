ba6f3492bb30bef627b7e34eaa10f7760df0f4c2
Jan Pluskal <jan.pluskal@gmail.com>
2022-02-08 07:18:37 +0100

    Allow SqlServerDbContextFactory to create DbContext with seeded data.

--- src/CookBook/CookBook.App/Factories/SqlServerDbContextFactory.cs
+++ src/CookBook/CookBook.App/Factories/SqlServerDbContextFactory.cs
@@ -6,21 +6,23 @@ namespace CookBook.App.Factories
     public class SqlServerDbContextFactory : IDbContextFactory<CookBookDbContext>
     {
         private readonly string _connectionString;
+        private readonly bool _seedTestingData;
 
-        public SqlServerDbContextFactory(string connectionString)
+        public SqlServerDbContextFactory(string connectionString, bool seedTestingData = false)
         {
             _connectionString = connectionString;
+            _seedTestingData = seedTestingData;
         }
 
         public CookBookDbContext CreateDbContext()
         {
             var optionsBuilder = new DbContextOptionsBuilder<CookBookDbContext>();
             optionsBuilder.UseSqlServer(_connectionString);
-            //optionsBuilder.UseInMemoryDatabase("aa");
+
             //optionsBuilder.LogTo(System.Console.WriteLine); //Enable in case you want to see tests details, enabled may cause some inconsistencies in tests
             //optionsBuilder.EnableSensitiveDataLogging();
 
-            return new CookBookDbContext(optionsBuilder.Options, seedTestingData:true);
+            return new CookBookDbContext(optionsBuilder.Options, _seedTestingData);
         }
     }
 }
