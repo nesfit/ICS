41f11b7453f8f6a5b85c83798013d8f0443830cd
Jan Pluskal <ipluskal@fit.vutbr.cz>
2022-02-15 14:29:38 +0100

    Add SQLite DB for testing.

--- src/CookBook/CookBook.BL.Tests/CRUDFacadeTestsBase.cs
+++ src/CookBook/CookBook.BL.Tests/CRUDFacadeTestsBase.cs
@@ -20,8 +20,9 @@ public class  CRUDFacadeTestsBase : IAsyncLifetime
         XUnitTestOutputConverter converter = new(output);
         Console.SetOut(converter);
 
-        DbContextFactory = new DbContextTestingInMemoryFactory(GetType().Name, seedTestingData: true);
+        // DbContextFactory = new DbContextTestingInMemoryFactory(GetType().Name, seedTestingData: true);
         // DbContextFactory = new DbContextLocalDBTestingFactory(GetType().FullName!, seedTestingData: true);
+        DbContextFactory = new DbContextSQLiteTestingFactory(GetType().FullName!, seedTestingData: true);
 
         UnitOfWorkFactory = new UnitOfWorkFactory(DbContextFactory);
 
