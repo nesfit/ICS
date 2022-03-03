b1eb837b58e928ebf4cc8f0609d7a7ff25733df8
Jan Pluskal <pluskal@netsearch.cz>
2022-03-03 07:40:28 +0100

    Remove old static L04 site.

--- Lectures/Lecture_04/_site/assets/sln/EntityFramework/School.DAL/Entities/IEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace School.DAL.Entities
-{
-    public interface IEntity
-    {
-        Guid Id { get; set; }
-    }
-}
--- Lectures/Lecture_04/_site/assets/sln/EntityFramework/School.DAL/Factories/INamedDbContextFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace School.DAL.Factories
-{
-    public interface INamedDbContextFactory<out TDbContext> where TDbContext : DbContext
-    {
-        TDbContext Create();
-    }
-}
