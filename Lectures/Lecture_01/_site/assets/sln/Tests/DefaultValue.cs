2d2c56c59c9cefc9aba833fd04215ec859fe444b
Jan Pluskal <ipluskal@vut.cz>
2023-02-08 20:01:57 +0100

    Update L01 static site.

--- Lectures/Lecture_01/_site/assets/sln/Tests/DefaultValue.cs
+++ Lectures/Lecture_01/_site/assets/sln/Tests/DefaultValue.cs
@@ -1,5 +1,9 @@
 using System;
+using Tests;
 using Xunit;
+#pragma warning disable CS0168 // Variable is declared but never used
+#pragma warning disable CS0649 // Field 'DefaultValue._y' is never assigned to, and will always have its default value 0	Tests	C:\Users\pluskal\source\repos\ICS\Lectures\Lecture_01\assets\sln\Tests\DefaultValue.cs  11  Active
+#pragma warning disable IDE0044 // Add readonly modifier
 
 namespace Tests
 {
