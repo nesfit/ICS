20fc4c4e7f4d0aeb6f887993c607ea11d8cb7a53
jasho <roman.jasek@hotmail.com>
2023-03-04 13:14:25 +0100

    Added displaying of error message if ingredient deletion failed

---
+++ src/CookBook/CookBook.App/Services/AlertService.cs
@@ -0,0 +1,14 @@
+﻿namespace CookBook.App.Services;
+
+public class AlertService : IAlertService
+{
+    public async Task DisplayAsync(string title, string message)
+    {
+        var displayAlert = Application.Current?.MainPage?.DisplayAlert(title, message, "OK");
+        
+        if (displayAlert is not null)
+        {
+            await displayAlert;
+        }
+    }
+}
---
+++ src/CookBook/CookBook.App/Services/Interfaces/IAlertService.cs
@@ -0,0 +1,6 @@
+﻿namespace CookBook.App.Services;
+
+public interface IAlertService
+{
+    Task DisplayAsync(string title, string message);
+}
