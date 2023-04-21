bf9073c5da28fced04290d43f64428b323ae1413
Jan Pluskal <ipluskal@vut.cz>
2023-04-21 08:28:13 +0200

    Build L11 static site

---
+++ Lectures/Lecture_11/_site/assets/sln/ParallelProgramming.Sampels/Asynchronous/AsynchronousParallelismExceptions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ParallelProgramming.Samples.Asynchronous
+{
+    public class AsynchronousParallelismExceptions
+    {
+        private readonly ITestOutputHelper output;
+
+        public AsynchronousParallelismExceptions(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        private readonly SemaphoreSlim testOutputSemaphore = new(1);
+
+        private Task ThrowExceptionAsync()
+        {
+            throw new Exception("Bum!");
+        }
+
+
+        [Fact]
+        public async Task CatchAsyncExceptions()
+        {
+            await Assert.ThrowsAsync<Exception>(async () => await ThrowExceptionAsync());
+        }
+        
+        [Fact]
+        public void CatchAsyncExceptions_ThrowsSynchronously()
+        {
+            Task task = null;
+            try
+            {
+                task = ThrowExceptionAsync();
+            }
+            catch (Exception e)
+            {
+                // skip
+                // this is not what you expect
+            }
+            
+            Assert.Null(task);
+        }
+        
+        [Fact]
+        public async Task CatchAsyncExceptions_ThrowsAsyncSynchronously()
+        {
+            var task = Task.Run(ThrowExceptionAsync);
+            await Assert.ThrowsAsync<Exception>(async () => await task);
+        }
+    }
+}
