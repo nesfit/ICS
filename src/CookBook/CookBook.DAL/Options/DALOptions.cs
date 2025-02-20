def629b54af5c0f35fd45da5c76973fda8d8b8b5
Jan Pluskal <ipluskal@vut.cz>
2025-02-20 09:27:18 +0100

    chore(CookBook): remove missing options folder

--- src/CookBook/CookBook.DAL/Options/DALOptions.cs
+++ src/CookBook/CookBook.DAL/Options/DALOptions.cs
@@ -5,6 +5,7 @@ public record DALOptions
     public required string DatabaseDirectory { get; init; }
     public string DatabaseName { get; init; } = null!;
     public string DatabaseFilePath => Path.Combine(DatabaseDirectory, DatabaseName!);
+
     /// <summary>
     /// Deletes database before application startup
     /// </summary>
