9549e7d4ebbabb320edcb4260f19d493185da306
Jan Pluskal <ipluskal@vut.cz>
2026-02-09 18:02:55 +0100

    fix(cookbook): update maui root page and alerts

--- src/CookBook/CookBook.App/App.xaml.cs
+++ src/CookBook/CookBook.App/App.xaml.cs
@@ -4,10 +4,17 @@ namespace CookBook.App;
 
 public partial class App : Application
 {
+    private readonly IServiceProvider _serviceProvider;
+
     public App(IServiceProvider serviceProvider)
     {
+        _serviceProvider = serviceProvider;
         InitializeComponent();
+    }
 
-        MainPage = serviceProvider.GetRequiredService<AppShell>();
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        var shell = _serviceProvider.GetRequiredService<AppShell>();
+        return new Window(shell);
     }
 }
