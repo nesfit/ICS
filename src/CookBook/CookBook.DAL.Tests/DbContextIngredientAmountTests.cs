f34846358f07c2bb80513f887452b1a83c2913fa
Jan Pluskal <ipluskal@vut.cz>
2026-03-03 17:48:13 +0100

    test(cookbook): verify unique recipe-ingredient constraint

--- src/CookBook/CookBook.DAL.Tests/DbContextIngredientAmountTests.cs
+++ src/CookBook/CookBook.DAL.Tests/DbContextIngredientAmountTests.cs
@@ -87,4 +87,20 @@ public class DbContextIngredientAmountTests(ITestOutputHelper output) : DbContex
         //Assert
         Assert.False(await CookBookDbContextSUT.IngredientAmountEntities.AnyAsync(i => i.Id == baseEntity.Id));
     }
+
+    [Fact]
+    public async Task Add_DuplicateIngredientForRecipe_Throws()
+    {
+        // Arrange
+        var duplicate = IngredientAmountSeeds.IngredientAmountEntity1 with
+        {
+            Id = Guid.Parse("8d0e0946-5834-4f22-8d0b-436d0bc29de5"),
+            Recipe = null!,
+            Ingredient = null!
+        };
+
+        // Act & Assert
+        CookBookDbContextSUT.IngredientAmountEntities.Add(duplicate);
+        await Assert.ThrowsAsync<DbUpdateException>(() => CookBookDbContextSUT.SaveChangesAsync());
+    }
 }
