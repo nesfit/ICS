eb08910a2e670379476bfad9edb757d3d0829613
Jan Pluskal <ipluskal@vut.cz>
2026-03-04 06:47:35 +0100

    chore(cookbook): sync EF snapshot and add drift test

---
+++ src/CookBook/CookBook.DAL/Migrations/20260304054427_SyncModelSnapshot.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CookBook.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncModelSnapshot : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_IngredientAmountEntities_RecipeId",
+                table: "IngredientAmountEntities");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngredientAmountEntities_RecipeId_IngredientId",
+                table: "IngredientAmountEntities",
+                columns: new[] { "RecipeId", "IngredientId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_IngredientAmountEntities_RecipeId_IngredientId",
+                table: "IngredientAmountEntities");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngredientAmountEntities_RecipeId",
+                table: "IngredientAmountEntities",
+                column: "RecipeId");
+        }
+    }
+}
