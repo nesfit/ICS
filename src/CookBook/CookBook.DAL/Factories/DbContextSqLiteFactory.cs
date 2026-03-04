ab038cf9509d882bccefd28c140f3e22694cac32
Jan Pluskal <ipluskal@vut.cz>
2026-03-04 06:26:47 +0100

    fix(cookbook): disable shared sqlite cache in context factory

--- src/CookBook/CookBook.DAL/Factories/DbContextSqLiteFactory.cs
+++ src/CookBook/CookBook.DAL/Factories/DbContextSqLiteFactory.cs
@@ -10,7 +10,7 @@ public class DbContextSqLiteFactory : IDbContextFactory<CookBookDbContext>
     {
         ////May be helpful for ad-hoc testing, not drop in replacement, needs some more configuration.
         //builder.UseSqlite($"Data Source =:memory:;");
-        _contextOptionsBuilder.UseSqlite($"Data Source={databaseName};Cache=Shared");
+        _contextOptionsBuilder.UseSqlite($"Data Source={databaseName}");
 
         ////Enable in case you want to see tests details, enabled may cause some inconsistencies in tests
         //_contextOptionsBuilder.EnableSensitiveDataLogging();
